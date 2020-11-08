49962956f47cce7a27044c33f455217121a6114b
Lukáš Matuška <lukynmatuska@gmail.com>
2020-11-08 17:00:50 +0100

    Send message to specific channel every minute

--- index.js
+++ index.js
@@ -6,19 +6,30 @@ const config = require('./config')
 const Discord = require('discord.js')
 const client = new Discord.Client()
 const fs = require('fs')
+var CronJob = require('cron').CronJob
+
+function sendMessageToChannel() {
+  client.channels.cache.forEach(channel => {
+    if (channel.id === process.env.DISCORD_CHANNEL) {
+      channel.send(new Date().getTime())
+    }
+  })
+}
 
 client.once('ready', () => {
   console.log('Ready!')
-  client.user.setActivity('', { type: 'PLAYING' })
-  console.log(client.channels)
-  client.channels.cache.forEach(channel => {
-    if (channel.id === process.env.DISCORD_CHANNEL) {
-      console.log(channel)
-    }
+  client.user.setActivity(process.env.BOT_ACTIVITY_NAME, {
+    type: process.env.BOT_ACTIVITY_OPTION_TYPE
   })
+    .then(presence => {
+      console.log(presence.activities[0].type + ' ' + presence.activities[0].name)
+    })
+    .catch(console.error)
+  new CronJob(process.env.CRON_JOB_TIMING, sendMessageToChannel, null, true, process.env.UTC_LOCATION).start()
 })
 
-client.on('message', message => {
+
+/*client.on('message', message => {
   //   console.log(message.content)
   if (message.content === `${config.prefix}ping`) {
     // send back "Pong." to the channel the message was sent in
@@ -26,7 +37,7 @@ client.on('message', message => {
   } else if (message.content === `${config.prefix}server`) {
     message.channel.send(`Server name: ${message.guild.name}\nTotal members: ${message.guild.memberCount}`)
   }
-})
+})*/
 
 // Bot login
 client.login(process.env.DISCORD_TOKEN)
