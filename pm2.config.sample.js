8921449957a223f6b0b21fec95f279868b78c92b
Lukas Matuska <lukynmatuska@gmail.com>
2021-03-05 12:17:37 +0100

    Update PM2 Ecosystem file

--- pm2.config.sample.js
+++ pm2.config.sample.js
@@ -2,7 +2,7 @@ module.exports = {
   apps : [
       {
         name: "nnn-discord-bot",
-        script: "./app.js",
+        script: "./index.js",
         watch: true,
         env: {
             "NODE_ENV": "development"
